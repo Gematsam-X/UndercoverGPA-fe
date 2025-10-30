6b06dcbe98f57c3f1058e10d8609a28f281abd0e
Gionatan Venturi <gionatan.venturi@gmail.com>
2025-10-30 11:19:39 +0100

    feat: add initial login and register pages and create the main component with a boilerplate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { EmailFound } from './email-found/email-found';
+import { Homepage } from './homepage/homepage';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+	{ path: 'login', component: Login },
+	{ path: 'register', component: Register },
+	{ path: 'email-found', component: EmailFound },
+	{ path: 'home', component: Homepage },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, FormsModule], 
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] 
 })
 export class App {
   protected readonly title = signal('UndercoverGPA-fe');
